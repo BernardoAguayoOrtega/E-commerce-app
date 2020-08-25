2341166a3fd0b23264618314a798630d018e0112
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-24 21:22:34 -0500

    create reac router provider and set up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+//import react dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import browser router from react-router-dom that allow us to implement as a provider trough our app
+import { BrowserRouter as Router } from 'react-router-dom';
+//import styles
 import '../src/assets/styles/index.css';
+//import components
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root'),
+);
