90dfdbb2c92e289c955f606876c9cc7489047454
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-24 21:11:53 -0500

    set dependecies

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Cart from './pages/Cart';
 import Photos from './pages/Photos';
 
 function App() {
-	return (
-		<div>
-			<Header />
-			<h1>Home Page</h1>
-		</div>
-	);
+  return (
+    <div>
+      <Header />
+      <h1>Home Page</h1>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function Cart() {
-	return (
-		<main className='cart-page'>
-			<h1>Check out</h1>
-		</main>
-	);
+  return (
+    <main className='cart-page'>
+      <h1>Check out</h1>
+    </main>
+  );
 }
 
 export default Cart;
