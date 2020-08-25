e085a701c6ad5f8703e503fc01cd035825b2d2b0
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-25 08:48:21 -0500

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+//import react
 import React from 'react';
+//import Switch and Route components from react router dom
+import {Switch, Route} from 'react-router-dom';
+//import components
 import Header from './components/Header';
 import Cart from './pages/Cart';
 import Photos from './pages/Photos';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      <h1>Home Page</h1>
-    </div>
+
+      <Switch>
+        <Route exact path='/'>
+          <Photos />
+        </Route>
+        <Route exact path='/cart'>
+          <Cart />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
+//import react
 import React from 'react';
-
+//import Link component from react router dom, allow us to set values to switch
+import {Link} from 'react-router-dom';
 function Header() {
   return (
     <header>
-      <h2>Pic Some</h2>
-      <i className='ri-shopping-cart-line ri-fw ri-2x' />
+      <Link to='/'> <h2>Pic Some</h2> </Link>
+      <Link to='/cart'> <i className='ri-shopping-cart-line ri-fw ri-2x' /> </Link>
     </header>
   );
 }
