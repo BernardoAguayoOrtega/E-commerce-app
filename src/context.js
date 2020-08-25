f0a107df009bdee425eebd1a0fa03aa028750799
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-25 09:14:58 -0500

    use useState hook to create state

--- src/context.js
+++ src/context.js
@@ -1,9 +1,12 @@
-//import react
-import React from 'react';
+//import react and its hooks
+import React, { useState } from 'react';
 //create react context
 const Context = React.createContext();
 //functional component that renders the provider context
 function ContextProvider({ children }) {
-  return <Context.Provider value=''>{children}</Context.Provider>;
+  // eslint-disable-next-line no-unused-vars
+  const [photos, setPhotos] = useState([])
+
+  return <Context.Provider value={{ photos }}>{children}</Context.Provider>;
 }
 export { Context, ContextProvider };
