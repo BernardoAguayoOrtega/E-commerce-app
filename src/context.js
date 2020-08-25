9bd3f51abf1cdffc4a36a6ebb9955a5ea4b83ae6
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-25 09:46:58 -0500

    make api request to get photos

--- src/context.js
+++ src/context.js
@@ -1,11 +1,21 @@
 //import react and its hooks
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 //create react context
 const Context = React.createContext();
 //functional component that renders the provider context
 function ContextProvider({ children }) {
-  // eslint-disable-next-line no-unused-vars
+  //use state hook that allow us to have state into a functional component
   const [photos, setPhotos] = useState([])
+  //const for url
+  const url = 'https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json'
+  //use effect hook that allow us to have component life cycle into a functional component
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => setPhotos(json));
+  }, []);
+
+  console.log(photos)
 
   return <Context.Provider value={{ photos }}>{children}</Context.Provider>;
 }
