538ff5fecd1067f720d32ec123df9dcb1bda615a
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-25 20:16:54 -0500

    showCartEMpty function Change the cart icon in the header to display the full cart icon if there are any items in the cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
-//import react
-import React from 'react';
+//import react and useContext hook
+import React, { useContext } from 'react';
+
+//import context
+import {Context} from '../context'
 
 //import Link component from react router dom, allow us to set values to switch
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const { cartItems } = useContext(Context)
+
+  // eslint-disable-next-line eqeqeq
+  const showCartEmpty = () => (cartItems.length < 1 || cartItems == undefined ?
+    <i className='ri-shopping-cart-line ri-fw ri-2x' /> :
+    <i className='ri-shopping-cart-fill ri-fw ri-2x' />)
   return (
     <header>
       <Link to='/'> <h2>Pic Some</h2> </Link>
-      <Link to='/cart'> <i className='ri-shopping-cart-line ri-fw ri-2x' /> </Link>
+      <Link to='/cart'> {showCartEmpty()} </Link>
     </header>
   );
 }
