55bbc202a423777d5034af178b504225acdebf2d
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-25 21:26:18 -0500

    add functionality to delete button from cart component

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,15 @@
-import React from 'react'
+//import react and use context hook
+import React, { useContext } from 'react'
+
+//import context
+import { Context } from '../context'
 
 function CartItem({ item }) {
+  const { removeToCart } = useContext(Context)
+
   return (
     <div className='cart-item'>
-      <i className='ri-delete-bin-line' />
+      <i className='ri-delete-bin-line' onClick={() => removeToCart(item)}/>
       <img src={item.url} width='130px' />
       <p>$5.99</p>
     </div>
